2a94d62a6e1226aa0cb2a901242a4de0f442cc5e
wade <wade@example.com>
2023-04-25 15:42:55 +0800

    res

--- lib/app_store_connect_api/review_submission_item_create_request.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module AppStoreConnectApi
-  class ReviewSubmissionItemCreateRequest < Request::Body
-    data do
-      type 'reviewSubmissionItems'
-    end
-  end
-end
